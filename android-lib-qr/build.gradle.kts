28e1d7f56d2e0913ef2785fbda188a35f46d89ad
lzq <761048783@qq.com>
2023-06-26 13:49:41 +0800

    v1.0.0

---
+++ android-lib-qr/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    `maven-publish`
+}
+
+android {
+    namespace = "com.lsn.lib.qr"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "com.lsn.lib"
+                artifactId = "qr"
+                version = "1.0.0"
+//                from(components["java"])
+                afterEvaluate { artifact(tasks.getByName("bundleReleaseAar")) }
+            }
+        }
+    }
+}
+
+dependencies {
+    implementation(files("libs\\core-3.3.3.jar"))
+}
